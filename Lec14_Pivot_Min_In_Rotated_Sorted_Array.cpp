2e1099e424c2fc29a1ca208e26343412c1fbbca0
pd27dixit <98444213+pd27dixit@users.noreply.github.com>
2023-08-05 20:40:52 +0530

    Create Lec14_Pivot_Min_In_Rotated_Sorted_Array.cpp

---
+++ Lec14_Pivot_Min_In_Rotated_Sorted_Array.cpp
@@ -0,0 +1,13 @@
+int getPivotIndex(int arr[],int n)
+{
+  int s=0,e=n-1;
+  int mid=s+(e-s)/2;
+  while(s<e)
+    {
+      if(arr[mid] >= arr[0]) s=mid+1;
+      else e=mid;
+
+      mid=s+(e-s)/2;
+    }
+  return s;
+}
