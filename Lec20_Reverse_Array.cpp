5ea0500c41cc9087969cad18fbd2341112b51e03
pd27dixit <98444213+pd27dixit@users.noreply.github.com>
2023-08-10 23:29:53 +0530

    Update Lec20_Reverse_Array.cpp

--- Lec20_Reverse_Array.cpp
+++ Lec20_Reverse_Array.cpp
@@ -1,1 +1,12 @@
+void reverseArray(vector<int> &arr , int m)
+{
+  int start = m+1  ; // question asked to start after mth index, normally used start=0
+  int end = arr.size() - 1;
 
+  while(start <= end)
+    {
+      swap(arr[start],arr[end]);
+      start++;
+      end--;
+    }
+}
