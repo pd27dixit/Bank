f061314696c31fed6aa99cdbd9ce4e24c3b19dc1
pd27dixit <98444213+pd27dixit@users.noreply.github.com>
2023-08-10 23:39:52 +0530

    Update Lec20_Moves_zeros_end.cpp

--- Lec20_Moves_zeros_end.cpp
+++ Lec20_Moves_zeros_end.cpp
@@ -1,1 +1,14 @@
-
+void pushZerosAtEnd(vector<int> &arr) 
+{
+	// Write your code here.
+	// shift all nonzero elements shift to left
+	int nonzero=0;
+	for(int j=0;j<arr.size();j++)
+	{
+		if(arr[j]!=0)
+		{
+			swap(arr[nonzero],arr[j]);
+			nonzero++;
+		}
+	}
+}
