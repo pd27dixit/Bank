27e5a0119d851464c18b221237ada5c755a930b4
pd27dixit <98444213+pd27dixit@users.noreply.github.com>
2023-08-11 10:29:20 +0530

    Create Lec21_Sum_of_2_arrays.cpp

---
+++ Lec21_Sum_of_2_arrays.cpp
@@ -0,0 +1,46 @@
+vector<int> findArraySum(vector<int> &a, int n, vector<int> &b,int m)
+{
+    int i= n-1;
+    int j=m-1;
+    int carry=0;
+    vector<int> ans;
+
+    while(i>=0 && j>=0)
+    {
+        int sum = a[i] + b[j] + carry;
+        carry = sum/10;
+        sum=sum%10;
+        ans.push_back(sum);
+        i--;
+        j--;
+    }
+
+    while(i>=0)
+    {
+        int sum = a[i]+carry;
+        carry = sum/10;
+        sum=sum%10;
+        ans.push_back(sum);
+        i--;
+    }
+
+    while(j>=0)
+    {
+        int sum = b[j] + carry;
+        carry = sum/10;
+        sum=sum%10;
+        ans.push_back(sum);
+        j--;
+    }
+
+    while(carry!=0)
+    {
+        int sum = b[j]+carry;
+		carry=sum/10;
+		sum=sum%10;
+		ans.push_back(sum);
+	}
+	reverse(ans.begin(),ans.end());
+	return ans;
+
+}
